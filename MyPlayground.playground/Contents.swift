4d768066cfb6c57697b5e8559c718975b4cba374
GkFool <1964661143@qq.com>
2016-01-04 21:03:54 +0800

    Demo for kinds of swift Int type

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//: Int 整形
+
+var num1:Int
+
+num1 = 20
+
+Int.max
+
+Int.min
+
+Int16.max
+
+Int16.min
+
+UInt64.max
+
+UInt64.min
+
+
+
+num1 = 0x14
+
+
+num1 = 0o24
+
+num1 = 0b10100
+
+num1 = 1_000_000
+
+
+
+
+
+
+
+
