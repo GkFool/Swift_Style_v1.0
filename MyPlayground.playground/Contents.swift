b4ea4b59c7b73a242aa24b688a348843a753687f
GkFool <1964661143@qq.com>
2016-01-04 21:27:43 +0800

    Float & Double

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,31 +7,50 @@ var str = "Hello, playground"
 //: Int 整形
 
 var num1:Int
-
 num1 = 20
-
 Int.max
-
 Int.min
-
 Int16.max
-
 Int16.min
-
 UInt64.max
-
 UInt64.min
-
-
-
 num1 = 0x14
-
-
 num1 = 0o24
-
 num1 = 0b10100
-
 num1 = 1_000_000
+
+//: Float Double 浮点型
+
+var num2: Float = 1/6
+
+var num3: Double = 1/6
+
+print(num2,terminator:"")
+
+print(num3,terminator:"")
+
+var PI = 0.31415926e1
+
+
+
+var num4 = 1000.5
+
+num4.dynamicType
+
+num4 = Double(num1) + Double(num2)
+
+
+
+
+//
+
+
+
+
+
+
+
+
 
 
 
